dcc36d22b43a65e8241d947c0815ae9ad9bcc4c9
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2023-10-22 19:29:13 +0800

    fix responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-8 lg:p-24 bg-slate-950 text-gray-100">
       <div>
         <h1 className="text-4xl font-bold text-center">Path Research</h1>
         <p className="text-xl">
@@ -35,32 +35,41 @@ function CSSPathResearch() {
   ];
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-8 w-full ">
-      {cards.map((card, i) => (
-        <div key={i} className="card aspect-[1/1] bg-gray-800 rounded-lg flex flex-col items-center justify-center">
-          <h2 className="text-xl font-bold m-2">{card.title}</h2>
-          <div className={`w-full h-full ${card.color} ${card.class} cursor-pointer`}></div>
-        </div>
-      ))}
-      <div className="aspect-[16/4] bg-gray-800 rounded-lg flex flex-col items-center justify-center">
-        <h2 className="text-xl font-bold m-2 mx-auto text-center">
-          <a href="https://gsap.com/resources/SVG" target="_blank" rel="noopener noreferrer" className="text-center text-white hover:text-blue-400">{`Complex animations with GSAP
-          https://gsap.com/resources/SVG`}</a>
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-8 mb-10 w-full">
+        {cards.map((card, i) => (
+          <div key={i} className="card aspect-[1/1] bg-gray-800 rounded-lg flex flex-col items-center justify-center">
+            <h2 className="text-xl font-bold m-2">{card.title}</h2>
+            <div className={`w-full h-full ${card.color} ${card.class} cursor-pointer`}></div>
+          </div>
+        ))}
+      </div>
+
+
+      <div className="m-2 mx-auto text-center flex-col ">
+        <h2 className='text-xl font-bold '>
+          Complex animations with GSAP
         </h2>
+        <a href="https://gsap.com/resources/SVG" target="_blank" rel="noopener noreferrer" className="text-center text-white hover:text-blue-400">
+
+          https://gsap.com/resources/SVG</a>
       </div>
-      <div className="aspect-[16/10] bg-gray-800 rounded-lg flex flex-col items-center justify-center">
-        <h2 className="text-xl font-bold m-2">Animating origin with GSAP</h2>
-        <div className={`w-full h-full`}>
-          <Image src="/origin-animation-gsap.gif" alt="examples of origin animation with GSAP" width={1200} height={679} />
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-8 w-full">
+        <div className="aspect-[16/10] bg-gray-800 rounded-lg flex flex-col items-center justify-center">
+          <h2 className="text-xl font-bold m-2">Animating origin with GSAP</h2>
+          <div className={`w-full h-full`}>
+            <Image src="/origin-animation-gsap.gif" alt="examples of origin animation with GSAP" width={1200} height={679} />
+          </div>
+        </div>
+        <div className="aspect-[16/10] bg-gray-800 rounded-lg flex flex-col items-center justify-center">
+          <h2 className="text-xl font-bold m-2">Animating shapes with GSAP</h2>
+          <div className={`w-full h-full`}>
+            <Image src="/animating-shapes-gsap.gif" alt="examples of origin animation with GSAP" width={1200} height={493} />
+          </div>
         </div>
       </div>
-      <div className="aspect-[16/9] bg-gray-800 rounded-lg flex flex-col items-center justify-center">
-        <h2 className="text-xl font-bold m-2">Animating shapes with GSAP</h2>
-        <div className={`w-full h-full`}>
-          <Image src="/animating-shapes-gsap.gif" alt="examples of origin animation with GSAP" width={1200} height={493} />
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 
