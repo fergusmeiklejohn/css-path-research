5eb9a3da1fdc17e54bd743f993dc45acbc8553b8
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2023-10-22 12:56:48 +0800

    pixel and % values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1 className="text-4xl font-bold text-center">CSS Path Research</h1>
+        <h1 className="text-4xl font-bold text-center">Path Research</h1>
         <p className="text-xl">
-          Some research into how CSS handles path shapes
+          Some research into how we handle path shapes in the browser
         </p>
       </div>
       <CSSPathResearch />
@@ -15,13 +15,26 @@ export default function Home() {
 }
 
 function CSSPathResearch() {
-  const cards = Array.from({ length: 7 }, (_, i) => i + 1);
+  const cards = [
+    {
+      title: "CSS Clip Path with Pixel values",
+      color: "bg-green-400",
+      class: "path1"
+    },
+    {
+      title: "CSS Clip Path with % values",
+      color: "bg-red-400",
+      class: "path2"
+    }
+    // add more cards here if needed
+  ];
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 w-full">
-      {cards.map((card) => (
-        <div className="card aspect-[1/1] max-w-[400px]" key={card}>
-          <h2 className="text-xl font-bold">Paths - {card}</h2>
-          <div className="w-full h-full bg-gray-200"></div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-8 w-full ">
+      {cards.map((card, i) => (
+        <div key={i} className="card aspect-[1/1] bg-gray-800 rounded-lg flex flex-col items-center justify-center">
+          <h2 className="text-xl font-bold m-2">{card.title}</h2>
+          <div className={`w-full h-full ${card.color} ${card.class} cursor-pointer`}></div>
         </div>
       ))}
     </div>
